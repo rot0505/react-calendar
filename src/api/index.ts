f06a6ff636cf72b916e2e9972d15893720774cc4
Rot <jamesdev0505@gmail.com>
2023-06-08 10:12:44 +0100

    add .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,25 @@
 import axios from "axios";
 import { IEvent, IEventCreate, TPartialEvent } from "types/event";
 
-const API_URL = 'https://61d8e2cfe6744d0017ba8cdc.mockapi.io/events';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const getEvents = async (): Promise<IEvent[]> => {
-  const response = await axios.get<IEvent[]>(API_URL);
+  const response = await axios.get<IEvent[]>(`${API_URL}/events`);
   return response.data;
 }
 
-const createEvent = async (eventData: IEventCreate): Promise<IEvent> => {
-  const response = await axios.post<IEvent>(`${API_URL}`, {
-    eventData
+const createEvent = async (event: IEventCreate): Promise<IEvent> => {
+  const response = await axios.post<IEvent>(`${API_URL}/event`, {
+    event
   });
   return response.data;
 }
 
-const deleteEvent = async (eventId: string) => axios.delete(`${API_URL}/${eventId}`);
+const deleteEvent = async (eventId: string) => axios.delete(`${API_URL}/event/${eventId}`);
 
-const updateEvent = async (eventId: string, eventData: TPartialEvent): Promise<IEvent> => {
-  const response = await axios.put<IEvent>(`${API_URL}/${eventId}`, {
-    eventData
+const updateEvent = async (eventId: string, event: TPartialEvent): Promise<IEvent> => {
+  const response = await axios.put<IEvent>(`${API_URL}/event/${eventId}`, {
+    event
   });
   return response.data;
 }
